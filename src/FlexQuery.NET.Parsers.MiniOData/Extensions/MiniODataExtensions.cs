0029a0c3932564d65f5bc21f9b082c874ae9b32e
Peter John Casasola <peterjohncasasola1214@gmail.com>
2026-07-05 16:06:11 +0800

    feat: add MiniODataRequest.ToQueryOptions() extension method

---
+++ src/FlexQuery.NET.Parsers.MiniOData/Extensions/MiniODataExtensions.cs
@@ -0,0 +1,27 @@
+using FlexQuery.NET.Models;
+
+namespace FlexQuery.NET.Parsers.MiniOData;
+
+/// <summary>
+/// Provides extension methods for working with MiniOData requests.
+/// </summary>
+public static class MiniODataExtensions
+{
+    /// <summary>
+    /// Converts a <see cref="MiniODataRequest"/> into a <see cref="QueryOptions"/> instance.
+    /// </summary>
+    /// <param name="request">
+    /// The MiniOData request to convert.
+    /// </param>
+    /// <returns>
+    /// A <see cref="QueryOptions"/> populated from the MiniOData request.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="request"/> is <see langword="null"/>.
+    /// </exception>
+    public static QueryOptions ToQueryOptions(this MiniODataRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        return ODataQueryParameterParser.Parse(request);
+    }
+}
