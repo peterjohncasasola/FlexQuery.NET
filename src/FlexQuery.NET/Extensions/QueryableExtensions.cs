5c91b547d86dff2366392019764e87b6340bca91
Peter John Casasola <49396986+peterjohncasasola@users.noreply.github.com>
2026-09-17 09:43:12 +0800

    refactor: remove static FlexQuery configuration from query extensions

--- src/FlexQuery.NET/Extensions/QueryableExtensions.cs
+++ src/FlexQuery.NET/Extensions/QueryableExtensions.cs
@@ -6,106 +6,54 @@ using FlexQuery.NET.Options;
 
 namespace FlexQuery.NET;
 
-/// <summary>
-/// Fluent extension methods on <see cref="IQueryable{T}"/> for applying
-/// <see cref="QueryOptions"/>.
-/// </summary>
 public static class QueryableExtensions
 {
-    private static IFlexQueryProcessor Processor => new FlexQueryProcessor(FlexQueryCore.DefaultOptions);
-    /// <summary>
-    /// Applies filter, sort, and paging in sequence.
-    /// </summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="options">The query options to apply.</param>
-    /// <returns>The filtered, sorted, and paged queryable.</returns>
-    public static IQueryable<T> Apply<T>(
-        this IQueryable<T> query, QueryOptions options)
+    public static IQueryable<T> Apply<T>(this IQueryable<T> query, QueryOptions options)
         => QueryBuilder.Apply(query, options);
 
-    /// <summary>Applies only the filter predicate.</summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="options">The query options containing the filter.</param>
-    /// <returns>The filtered queryable.</returns>
-    public static IQueryable<T> ApplyFilter<T>(
-        this IQueryable<T> query, QueryOptions options)
+    public static IQueryable<T> ApplyFilter<T>(this IQueryable<T> query, QueryOptions options)
         => QueryBuilder.ApplyFilter(query, options);
 
-    /// <summary>Applies only the sort instructions.</summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="options">The query options containing the sort instructions.</param>
-    /// <returns>The sorted queryable.</returns>
-    public static IQueryable<T> ApplySort<T>(
-        this IQueryable<T> query, QueryOptions options)
+    public static IQueryable<T> ApplySort<T>(this IQueryable<T> query, QueryOptions options)
         => QueryBuilder.ApplySort(query, options);
 
-    /// <summary>Applies only paging (skip/take).</summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="options">The query options containing paging parameters.</param>
-    /// <returns>The paged queryable.</returns>
-    public static IQueryable<T> ApplyPaging<T>(
-        this IQueryable<T> query, QueryOptions options)
+    public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, QueryOptions options)
         => QueryBuilder.ApplyPaging(query, options);
 
-    /// <summary>
-    /// Applies projection (select) dynamically and returns an IQueryable of objects.
-    /// If no select is specified, returns the original query cast to object.
-    /// </summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="options">The query options containing projection settings.</param>
-    /// <returns>A queryable of projected objects.</returns>
-    public static IQueryable<object> ApplySelect<T>(
-        this IQueryable<T> query, QueryOptions options)
+    public static IQueryable<object> ApplySelect<T>(this IQueryable<T> query, QueryOptions options)
         => QueryBuilder.ApplySelect(query, options);
 
-    /// <summary>
-    /// Parses a <see cref="FlexQueryParameters"/>, validates it against server rules,
-    /// and applies it to the query to return a paged result set.
-    /// </summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="parameters">The OpenAPI-friendly DTO containing user parameters.</param>
-    /// <param name="configure">Optional configuration for server-side security and execution rules.</param>
     public static QueryResult<object> FlexQuery<T>(
         this IQueryable<T> query,
         FlexQueryParameters parameters,
+        FlexQueryOptions? global = null,
         Action<QueryExecutionOptions>? configure = null)
     {
+        global ??= new FlexQueryOptions();
         var exec = new QueryExecutionOptions();
+        global.ApplyTo(exec);
         configure?.Invoke(exec);
+        global.Freeze();
 
-        var effectiveSyntax = exec.QuerySyntax ?? FlexQueryCore.DefaultOptions.DefaultQuerySyntax;
+        var effectiveSyntax = exec.QuerySyntax ?? global.DefaultQuerySyntax;
         var options = parameters.ToQueryOptions(effectiveSyntax);
-        return Processor.ExecuteAsync(query, options, exec)
-            .GetAwaiter()
-            .GetResult();
+        return new FlexQueryProcessor(global).ExecuteAsync(query, options, exec)
+            .GetAwaiter().GetResult();
     }
 
-    /// <summary>
-    /// Parses a <see cref="QueryOptions"/>, validates it against server rules,
-    /// and applies it to the query to return a paged result set.
-    /// </summary>
-    /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="query">The source queryable.</param>
-    /// <param name="queryOptions">The query options.</param>
-    /// <param name="configure">Optional configuration for server-side security and execution rules.</param>
     public static QueryResult<object> FlexQuery<T>(
         this IQueryable<T> query,
         QueryOptions queryOptions,
+        FlexQueryOptions? global = null,
         Action<QueryExecutionOptions>? configure = null)
     {
+        global ??= new FlexQueryOptions();
         var exec = new QueryExecutionOptions();
+        global.ApplyTo(exec);
         configure?.Invoke(exec);
-        
-        return Processor.ExecuteAsync(query, queryOptions, exec)
-            .GetAwaiter()
-            .GetResult();
-        
+        global.Freeze();
+
+        return new FlexQueryProcessor(global).ExecuteAsync(query, queryOptions, exec)
+            .GetAwaiter().GetResult();
     }
-    
 }
